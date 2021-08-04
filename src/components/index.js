496e8d113a1e9c78414e0953b1d292ffaadfb372
Amy Marais <amy.marais99@gmail.com>
2021-08-04 11:21:41 +0200

    add project

---
+++ src/components/index.js
@@ -0,0 +1,4 @@
+//cleaner way to export components
+export { default as Cards } from './Cards/Cards';
+export { default as Chart } from './Chart/Chart';
+export { default as CountryPicker } from './CountryPicker/CountryPicker';
